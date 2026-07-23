6100c9dc35da3084f45e0eac90e4c14aba48f6ba
Omar <omaraneudye@gmail.com>
2026-07-22 23:13:31 -0400

    Trabajando en el estilo del view Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,13 @@ import HeaderComponent from '../components/HeaderComponent.vue';
 
 <template>
     <HeaderComponent></HeaderComponent>
-    <h2>Home page</h2>
-    <p>Preparate para responder a estas trivias</p>
-    <router-link to="/trivias">Empezar trivia</router-link>
+    <div class="home-container">
+        <div class="home-text">
+            <h2 class="home-title">Home page</h2>
+            <p>Preparate para responder a estas trivias</p>
+            <router-link class="btn" to="/trivias">Empezar trivia</router-link>
+        </div>
+    </div>
 </template>
 
 <style></style>
